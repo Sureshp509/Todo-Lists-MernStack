70f7da7a1af2c307bc2a354b785e43e2087b164b
Sureshp509 <mail2suresh509@gmail.com>
2024-07-19 19:14:37 +0530

    Added react router navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { TodoProvider } from './context/TodoContext';
+import Navbar from './components/Layout/Navbar';
+import Home from './pages/Home';
+import LoginPage from './pages/Login';
+import RegisterPage from './pages/Register';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <AuthProvider>
+            <TodoProvider>
+                <Router>
+                    <Navbar />
+                    <div className="container">
+                        <Switch>
+                            <Route exact path="/" component={Home} />
+                            <Route exact path="/login" component={LoginPage} />
+                            <Route exact path="/register" component={RegisterPage} />
+                        </Switch>
+                    </div>
+                </Router>
+            </TodoProvider>
+        </AuthProvider>
+    );
+};
 
 export default App;
